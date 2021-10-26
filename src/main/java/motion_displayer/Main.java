eef17c1fd4ff96fe9e77834e21b5c4e11aa10dee
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-10-27 00:45:12 +0100

    Added initial resources needed for app and started designing UI

--- src/main/java/motion_displayer/Main.java
+++ src/main/java/motion_displayer/Main.java
@@ -1,23 +1,30 @@
 package motion_displayer;
 
-import motion_displayer.model.VideoFile;
-import motion_displayer.model.VideoProcessorHandler;
+import javafx.application.HostServices;
+import motion_displayer.view.App;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.opencv.core.Core;
 
 
-public class App {
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME );
-        Path in_path = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "f1.mp4");
-        Path out_path = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "f1_out.mp4");
-        System.out.printf("PROCESSING VIDEO PATH - %s%n", in_path.toString());
-        VideoFile video = new VideoFile(in_path, out_path, 40, 20, 0, false);
-        VideoProcessorHandler video_processor = new VideoProcessorHandler(video);
-        video_processor.process();
-        System.out.printf("COMPLETED VIDEO PATH - %s%n", out_path.toString());
+        Path dll = Paths.get(System.getenv("SystemDrive"), "opencv", "build", "java", "x64", "opencv_java453.dll");
+        System.load(dll.toString());
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        HostServices host_services = getHostServices();
+        Application.setUserAgentStylesheet(STYLESHEET_MODENA);
+        App window = new App(host_services, 1000, 800);
+        stage.setTitle("Motion Displayer");
+        stage.setResizable(false);
+        stage.setScene(window.getScene());
+        stage.show();
     }
 }
--- src/main/java/motion_displayer/view/App.java
+++ src/main/java/motion_displayer/view/App.java
@@ -1,20 +1,67 @@
 package motion_displayer.view;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.application.HostServices;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
 
 
 public class App {
 
-    private Pane pane;
+    private HostServices host_services;
     private Scene scene;
+    private Pane root;
 
-    public App(int width, int height) {
-        this.pane = new Pane();
-        this.scene = new Scene(this.pane, width, height);
+    public App(HostServices host_services, int width, int height) {
+        this.host_services = host_services;
+        this.root = this.makeRoot();
+        this.scene = new Scene(this.root, width, height);
+        System.out.println(String.valueOf(this.getClass().getResource("/styles.css")));
+        this.scene.getStylesheets().add(String.valueOf(this.getClass().getResource("/styles.css")));
     }
 
     public Scene getScene() {
         return this.scene;
     }
+
+    private Pane makeRoot() {
+        this.root = new AnchorPane();
+        Image github_image = new Image(String.valueOf(this.getClass().getClassLoader().getResource("github_logo_white.png")));
+        ImageView github_image_view = new ImageView(github_image);
+        github_image_view.setPreserveRatio(true);
+        github_image_view.setFitWidth(25);
+        github_image_view.setFitHeight(25);
+        String link = "https://github.com/Rhodri-Morgan/Motion-Displayer";
+        Hyperlink github_link = new Hyperlink(link);
+        github_link.setId("github_link");
+        github_link.setText("GitHub.com/Rhodri-Morgan/Motion-Displayer");
+        github_link.setBorder(Border.EMPTY);
+        github_link.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent t) {
+                host_services.showDocument(link);
+            }
+        });
+        AnchorPane.setTopAnchor(github_image_view, 10.0);
+        AnchorPane.setRightAnchor(github_image_view, 10.0);
+        AnchorPane.setTopAnchor(github_link, 13.0);
+        AnchorPane.setRightAnchor(github_link, 40.0);
+        this.root.getChildren().addAll(github_image_view, github_link);
+        return this.root;
+    }
 }
+
+/**
+ * Root Pane for things applicable to all different stages of app
+ * Add pane as children of root for new scenes/stages of app
+ *
+ * LOOK AT STYLE SHEETS FOR JAVAFX
+ */
