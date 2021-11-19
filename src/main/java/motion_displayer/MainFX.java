c4157c07f220b1147f740e5c07428f3f3ba9ccfb
Rhodri Morgan <rhodrithomasmorgan@protonmail.com>
2021-11-19 00:12:50 +0000

    Adding logo and building executable instructions

--- src/main/java/motion_displayer/MainFX.java
+++ src/main/java/motion_displayer/MainFX.java
@@ -1,6 +1,11 @@
 package motion_displayer;
 
 import motion_displayer.view.AppStateController;
+import java.util.Objects;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import javafx.application.Application;
@@ -12,13 +17,39 @@ import javafx.scene.image.Image;
 public class MainFX extends Application {
 
     public static void main(String[] args) {
-        Path dll = Paths.get(System.getenv("SystemDrive"), "opencv", "build", "java", "x64", "opencv_java453.dll");
-        System.load(dll.toString());
         launch(args);
+    }
+
+    /*
+        Solution by Kurt Kaylor - https://stackoverflow.com/questions/4691095/java-loading-dlls-by-a-relative-path-and-hide-them-inside-a-jar
+     */
+    public static void loadJarDll(String name) throws IOException {
+        InputStream in = MainFX.class.getClassLoader().getResourceAsStream(name);
+        byte[] buffer = new byte[1024];
+        int read = -1;
+        File temp = new File(new File(System.getProperty("java.io.tmpdir")), name);
+        FileOutputStream fos = new FileOutputStream(temp);
+
+        while((read = in.read(buffer)) != -1) {
+            fos.write(buffer, 0, read);
+        }
+        fos.close();
+        in.close();
+
+        System.load(temp.getAbsolutePath());
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        String protocol = this.getClass().getResource("").getProtocol();
+        if (Objects.equals(protocol, "jar")) {
+            loadJarDll("opencv_java453.dll");
+        }
+        else if (Objects.equals(protocol, "file")) {
+            Path dll = Paths.get(System.getenv("SystemDrive"), "opencv", "build", "java", "x64", "opencv_java453.dll");     // Local Running
+            System.load(dll.toString());
+        }
+
         HostServices host_services = getHostServices();
         Application.setUserAgentStylesheet(STYLESHEET_MODENA);
         AppStateController window = new AppStateController(host_services, stage, 1000, 800);
